d56ed5d8acfbf0328266dfb139fe5aeec21c02f4
Yuri Blokhin <ultrablox@gmail.com>
2015-06-23 11:55:49 +0300

    Added first docs

---
+++ doc/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+mkdir ../tmp/tex
+cp * ../tmp/tex
+cd ../tmp/tex
+
+#Compile all files
+
+for file in ${CWD}/*.tex
+do
+    if [[ -f $file ]]; then
+        base_fname=$(basename $file)
+        pdflatex -shell-escape ${base_fname}
+
+        compiled_name=`basename "$file" | cut -d. -f1`.pdf
+        cp ${compiled_name} ${CWD}/../doc_compiled/
+    fi
+done
+
+cd ${CWD}
